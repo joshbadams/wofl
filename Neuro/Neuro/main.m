5daeab9c6c34db9a72be61a081c13342f95c28e5
joshbadams <joshbadams@gmail.com>
2023-04-11 01:01:04 -0400

    kjhg

---
+++ Neuro/Neuro/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Neuro
+//
+//  Created by Josh Adams on 4/9/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // Setup code that might create autoreleased objects goes here.
+	}
+	return NSApplicationMain(argc, argv);
+}
